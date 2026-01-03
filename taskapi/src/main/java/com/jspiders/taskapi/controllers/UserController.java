d437adc8020790a062e4ea90e5ef89613b3a1806
Madhu HS <madhu.s.ty@cars24.com>
2026-01-03 15:07:07 +0530

    controller and service integration updated all methods

--- taskapi/src/main/java/com/jspiders/taskapi/controllers/UserController.java
+++ taskapi/src/main/java/com/jspiders/taskapi/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.jspiders.taskapi.controllers;
 
+import com.jspiders.taskapi.data.users.AppUser;
 import com.jspiders.taskapi.data.users.CreateUserRequest;
 import com.jspiders.taskapi.services.AppUserService;
 import com.jspiders.taskapi.services.impl.AppUserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -24,6 +27,33 @@ public class UserController
     {
         System.out.println("this is UserController --> addUser()");
         ResponseEntity<String> response = appUserService.createUser(createUserRequest);
+        return response;
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateUser(){
+        System.out.println("this is UserController --> updateUser()");
+        ResponseEntity<String> response = appUserService.updateUser();
+        return response;
+    }
+
+    @DeleteMapping
+    ResponseEntity<String> deleteUser(String email,String mobile,String password){
+        System.out.println("this is UserController --> deleteUser()");
+        ResponseEntity<String> response = appUserService.deleteUser(email,mobile,password);
+        return response;
+    }
+
+    @GetMapping
+    ResponseEntity<List<AppUser>> getAllUsers(){
+        System.out.println("this is UserController --> getAllUsers()");
+        ResponseEntity<List<AppUser>> response = appUserService.getAllUsers();
+        return response;
+    }
+    @GetMapping
+    ResponseEntity<AppUser> getUserById(Long userId){
+        System.out.println("this is UserController --> getUserById()");
+        ResponseEntity<AppUser> response = appUserService.getUserById(userId);
         return response;
     }
 }
--- taskapi/src/main/java/com/jspiders/taskapi/services/impl/AppUserServiceImpl.java
+++ taskapi/src/main/java/com/jspiders/taskapi/services/impl/AppUserServiceImpl.java
@@ -3,6 +3,7 @@ package com.jspiders.taskapi.services.impl;
 import com.jspiders.taskapi.data.users.AppUser;
 import com.jspiders.taskapi.data.users.CreateUserRequest;
 import com.jspiders.taskapi.services.AppUserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -20,26 +21,54 @@ public class AppUserServiceImpl implements AppUserService
 
         //logics
 
-        return null;
+//        ResponseEntity<String> response = ResponseEntity.ok("User created ");
+//        return response;
+         return ResponseEntity
+                 .status(HttpStatus.CREATED)
+                 .body("User created");
     }
 
     @Override
     public ResponseEntity<String> updateUser() {
-        return null;
+        System.out.println("this is AppUserServiceImpl --> updateUser()");
+
+        //logics
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("User updated");
     }
 
     @Override
-    public ResponseEntity<String> deleteUser(String email, String mobile, String password) {
-        return null;
+    public ResponseEntity<String> deleteUser(String email, String mobile, String password)
+    {
+        System.out.println("this is AppUserServiceImpl --> deleteUser()");
+
+        //logics
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("User deleted");
     }
 
     @Override
-    public ResponseEntity<List<AppUser>> getAllUsers() {
-        return null;
+    public ResponseEntity<List<AppUser>> getAllUsers()
+    {
+        System.out.println("this is AppUserServiceImpl --> getAllUsers()");
+
+        //logics
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(null);
     }
 
     @Override
-    public ResponseEntity<AppUser> getUserById(Long userId) {
-        return null;
+    public ResponseEntity<AppUser> getUserById(Long userId)
+    {
+        System.out.println("this is AppUserServiceImpl --> getUserById()");
+
+        //logics
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(null);
     }
 }
